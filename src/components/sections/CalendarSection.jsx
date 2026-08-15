a672fd2ef4915f3a970204ddcda21ae5abe5304b
alberic13 <zaldealberic@gmail.com>
2026-08-15 09:20:48 +0700

    update

--- src/components/sections/CalendarSection.jsx
+++ src/components/sections/CalendarSection.jsx
@@ -23,7 +23,12 @@ export default function CalendarSection() {
               How to design your site footer like we did
             </h2>
             <p className={styles.description}>
-              Donec a eros justo. Fusce egestas tristique ultrices. Nam tempor, augue nec tincidunt molestie, massa nunc varius arcu, at scelerisque elit erat a magna. Donec quis erat at libero ultrices mollis. In hac habitasse platea dictumst. Vivamus vehicula leo dui, at porta nisi facilisis finibus. In euismod augue vitae nisi ultricies, non aliquet urna tincidunt. Integer in nisi eget nulla commodo faucibus efficitur quis massa. Praesent felis est, finibus et nisi ac, hendrerit venenatis libero. Donec consectetur faucibus ipsum id gravida.
+              Donec a eros justo. Fusce egestas tristique ultrices. Nam tempor, augue nec tincidunt <br />
+              molestie, massa nunc varius arcu, at scelerisque elit erat a magna. Donec quis erat at <br />
+              libero ultrices mollis. In hac habitasse platea dictumst. Vivamus vehicula leo dui, at porta <br />
+              nisi facilisis finibus. In euismod augue vitae nisi ultricies, non aliquet urna tincidunt. Integer <br />
+              in nisi eget nulla commodo faucibus efficitur quis massa. Praesent felis est, finibus et nisi <br />
+              ac, hendrerit venenatis libero. Donec consectetur faucibus ipsum id gravida.
             </p>
           </div>
           <div className={styles.ctaWrapper}>
